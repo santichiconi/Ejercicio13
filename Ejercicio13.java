aaa0deca4a60c5acab6dd6d04a1e94784022d7b3
“Santiago <santichiconi@gmail.com>
2023-03-30 23:50:10 -0300

    Ejercicio13

---
+++ Ejercicio13.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio13;
+
+/**
+ *
+ * @author chuPac
+ */
+public class Ejercicio13 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String[] equipo = new String[5];
+       
+        
+        for (int i = 0; i <=4 ; i++) {
+            
+                equipo[i] = "Manuel" + 3;
+          }
+         
+        for (int i = 0; i <= 4; i++) {
+             
+                System.out.println(equipo[i]);
+            
+        }
+    
+                }
